66ef983a23bee9371b4b3d3cdd72eef07e54522f
leandropetrucirodrigues <91751588+leandropetrucirodrigues@users.noreply.github.com>
2022-02-01 14:31:26 -0300

    Alterações para meu formato

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
 import appConfig from '../config.json';
+
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -33,16 +35,20 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  // const username = 'omariosouto';
-  const [username, setUsername] = React.useState('omariosouto');
+  //const username = 'leandropetrucirodrigues';
+  const [username,setUsername] = React.useState('');
+  const [nameBottomImage,setBottomImage] = React.useState('GitHub');
+  const [semImagem,setImage] = React.useState('https://avatars.githubusercontent.com/u/9919?s=280&v=4')
   const roteamento = useRouter();
-
+  
+  
   return (
     <>
+
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundColor: appConfig.theme.colors.primary['000'],
           backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/10/international-space-station-cupola.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
@@ -57,73 +63,71 @@ export default function PaginaInicial() {
               sm: 'row',
             },
             width: '100%', maxWidth: '700px',
-            borderRadius: '5px', padding: '32px', margin: '16px',
+            borderRadius: '20px', padding: '30px', margin: '16px',
             boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
+            backgroundColor: appConfig.theme.colors.novas[999],
           }}
         >
           {/* Formulário */}
           <Box
             as="form"
-            onSubmit={function (infosDoEvento) {
-              infosDoEvento.preventDefault();
-              console.log('Alguém submeteu o form');
-              roteamento.push('/chat');
-              // window.location.href = '/chat';
-            }}
+			onSubmit={function (event){
+				event.preventDefault();
+				console.log('ele clicou');
+				//window.location.href ='/chat';
+				roteamento.push('/chat');
+			}}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.novas[100] }}>
               {appConfig.name}
             </Text>
 
-            {/* <input
-                            type="text"
-                            value={username}
-                            onChange={function (event) {
-                                console.log('usuario digitou', event.target.value);
-                                // Onde ta o valor?
-                                const valor = event.target.value;
-                                // Trocar o valor da variavel
-                                // através do React e avise quem precisa
-                                setUsername(valor);
-                            }}
-                        /> */}
+			
+
             <TextField
-              value={username}
-              onChange={function (event) {
-                console.log('usuario digitou', event.target.value);
-                // Onde ta o valor?
-                const valor = event.target.value;
-                // Trocar o valor da variavel
-                // através do React e avise quem precisa
-                setUsername(valor);
-              }}
-              fullWidth
-              textFieldColors={{
-                neutral: {
-                  textColor: appConfig.theme.colors.neutrals[200],
-                  mainColor: appConfig.theme.colors.neutrals[900],
-                  mainColorHighlight: appConfig.theme.colors.primary[500],
-                  backgroundColor: appConfig.theme.colors.neutrals[800],
+				value={username}
+				placeholder='Digite o nome do usuário no GitHub'
+				onChange={function (event){
+					// console.log('usuario digitou',event.target.value)
+					//onde ta o valor
+					const valor = event.target.value;
+					// trocar o valor da variavel
+					setUsername(valor);
+					setBottomImage(valor)
+
+					if (valor.length > 2) {
+						setImage(`https://github.com/${valor}.png`);	
+					}else{
+						setBottomImage(`Github`)
+						setImage('https://avatars.githubusercontent.com/u/9919?s=280&v=4')
+					}
+				}}
+				fullWidth
+				textFieldColors={{
+					neutral: {
+					textColor: appConfig.theme.colors.novas[999],
+					mainColor: appConfig.theme.colors.neutrals[500],
+					mainColorHighlight: appConfig.theme.colors.primary[500],
+					backgroundColor: appConfig.theme.colors.novas[100],
                 },
               }}
             />
             <Button
-              type='submit'
-              label='Entrar'
-              fullWidth
-              buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
-              }}
-            />
+				type='submit'
+				label='Entrar'
+				fullWidth
+				buttonColors={{
+					contrastColor: appConfig.theme.colors.novas[999],
+					mainColor: appConfig.theme.colors.novas[300],
+					mainColorLight: appConfig.theme.colors.novas[100],
+					mainColorStrong: appConfig.theme.colors.novas[700],
+				}}	
+			/>
           </Box>
           {/* Formulário */}
 
@@ -136,31 +140,31 @@ export default function PaginaInicial() {
               alignItems: 'center',
               maxWidth: '200px',
               padding: '16px',
-              backgroundColor: appConfig.theme.colors.neutrals[800],
+              backgroundColor: appConfig.theme.colors.novas[400],
               border: '1px solid',
               borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: '10px',
+              borderRadius: '20px',
               flex: 1,
               minHeight: '240px',
             }}
           >
             <Image
               styleSheet={{
-                borderRadius: '50%',
+                borderRadius: '20px',
                 marginBottom: '16px',
               }}
-              src={`https://github.com/${username}.png`}
+              src={semImagem}
             />
             <Text
               variant="body4"
               styleSheet={{
                 color: appConfig.theme.colors.neutrals[200],
-                backgroundColor: appConfig.theme.colors.neutrals[900],
+                backgroundColor: appConfig.theme.colors.novas[999],
                 padding: '3px 10px',
-                borderRadius: '1000px'
+                borderRadius: '5px'
               }}
             >
-              {username}
+            {nameBottomImage}
             </Text>
           </Box>
           {/* Photo Area */}
